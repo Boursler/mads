27dd8e0db1f24fec1ef3d859514d3da92cc2dd37
Briana Oursler <briana.oursler@gmail.com>
2018-10-02 14:49:25 -0700

    Update naming convention

--- lib/math_lib.h
+++ lib/math_lib.h
@@ -1,4 +1,8 @@
-long sum (long, long);
+long add(long, long);
 long subtract(long, long);
 long multiply(long, long);
 long divide(long, long);
+long xor(long, long);
+long or(long, long);
+long sum(long*, long);
+long sum_c(long*, long);
